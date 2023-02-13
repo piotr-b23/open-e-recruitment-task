9e4927d309756dcb39db92083222917019983a80
piotrekkk23 <gppiotrekgp23@gmail.com>
2023-02-13 17:31:57 +0100

    header, return button, form with validation added

--- open-e-recruitment-task/src/app/app-routing.module.ts
+++ open-e-recruitment-task/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserSearchComponent } from './user/user-search/user-search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/user', pathMatch: 'full' },
+  { path: 'user', component: UserSearchComponent, children: [
+    {path: '', component: UserSearchComponent},
+  ] },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ open-e-recruitment-task/src/app/user/user-search/user-search.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-search',
+  templateUrl: './user-search.component.html',
+  styleUrls: ['./user-search.component.scss']
+})
+export class UserSearchComponent {
+
+
+  onSubmit():void {
+    //send get request
+  }
+
+}
