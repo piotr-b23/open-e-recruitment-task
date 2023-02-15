b715eb3833f4630b87da42c550157eb69c43c7aa
piotrekkk23 <gppiotrekgp23@gmail.com>
2023-02-15 20:16:09 +0100

    displaying images in container

--- open-e-recruitment-task/src/app/user/user-detail/image-list/image-item/image-item.component.ts
+++ open-e-recruitment-task/src/app/user/user-detail/image-list/image-item/image-item.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { DisplayImage } from '../../gallery/display-image.model';
+import { GalleryService } from '../../gallery/gallery.service';
 import { Image } from '../image.model';
 
 @Component({
@@ -10,8 +12,11 @@ export class ImageItemComponent {
 
   @Input() image!: Image;
 
-  onClickImage(imageID: number){
+  onClickImage(){
+    const tempImage: DisplayImage = new DisplayImage(this.image.title,this.image.url);
 
+    this.galleryService.pushToGallery(tempImage);
   }
 
+  constructor(private galleryService: GalleryService) { }
 }
