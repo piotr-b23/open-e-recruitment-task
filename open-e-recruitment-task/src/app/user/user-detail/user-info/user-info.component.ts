195ad7a8b305fe9a3261c0654cd9d8e6506c1862
piotrekkk23 <gppiotrekgp23@gmail.com>
2023-02-15 08:46:35 +0100

    displaying user info added

--- open-e-recruitment-task/src/app/user/user-detail/user-info/user-info.component.ts
+++ open-e-recruitment-task/src/app/user/user-detail/user-info/user-info.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../user.service';
 
 @Component({
   selector: 'app-user-info',
   templateUrl: './user-info.component.html',
   styleUrls: ['./user-info.component.scss']
 })
-export class UserInfoComponent {
+export class UserInfoComponent implements OnInit {
+
+  initials!: string;
+  user!: string;
+  address!: string;
+  city!: string;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.initials = this.userService.createInitials();
+    this.user = this.userService.foundUser!.name;
+    this.address = `${this.userService.foundUser!.address.street} ${this.userService.foundUser!.address.suite}`;
+    this.city = this.userService.foundUser!.address.city;
+  }
+
+
 
 }
