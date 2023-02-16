ba5d8cb5dfa956be238c7cb23626d693fd52a1b4
piotrekkk23 <gppiotrekgp23@gmail.com>
2023-02-16 13:10:41 +0100

    image list code cleanup

--- open-e-recruitment-task/src/app/user/user-detail/image-list/image-list.component.ts
+++ open-e-recruitment-task/src/app/user/user-detail/image-list/image-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Image } from './image.model';
