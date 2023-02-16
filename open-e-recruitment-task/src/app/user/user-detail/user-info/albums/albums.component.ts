ad2a0ab0abe3f35b75ea1a0ec2ce6e85addce684
piotrekkk23 <gppiotrekgp23@gmail.com>
2023-02-16 12:49:00 +0100

    added unsubscribe in album component

--- open-e-recruitment-task/src/app/user/user-detail/user-info/albums/albums.component.ts
+++ open-e-recruitment-task/src/app/user/user-detail/user-info/albums/albums.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { UserService } from 'src/app/user/user.service';
 import { Album } from './album.model';
 import { AlbumsService } from './albums.service';
@@ -14,11 +14,13 @@ export class AlbumsComponent implements OnInit, OnDestroy{
   isDone: boolean = true;
   limit!: number;
 
-  constructor(private albumService: AlbumsService, private router: Router, private userService: UserService) { }
+  albumSubscription!: Subscription;
+
+  constructor(private albumService: AlbumsService, private userService: UserService) { }
 
   ngOnInit(): void {
 
-    this.albumService.getAlbums(this.userService.foundUser!.id).subscribe(albums => {
+    this.albumSubscription = this.albumService.getAlbums(this.userService.foundUser!.id).subscribe(albums => {
       this.albums = albums;
       this.limit = this.albumService.limit;
     });
@@ -27,13 +29,14 @@ export class AlbumsComponent implements OnInit, OnDestroy{
 
   ngOnDestroy(): void{
     this.albumService.clean();
+    this.albumSubscription.unsubscribe();
   }
 
   //adjustPage: -1 for going back in list, limit for going up in list
   changeAlbum(adjustPage: number): void{
     if (this.isDone){
       this.isDone = false;
-      this.albumService.getSpecificAlbum(this.userService.foundUser!.id, this.albumService.startPosition + adjustPage, 1).subscribe(()=>{
+      this.albumSubscription = this.albumService.getSpecificAlbum(this.userService.foundUser!.id, this.albumService.startPosition + adjustPage, 1).subscribe(()=>{
         this.isDone = true;
       });
     }
