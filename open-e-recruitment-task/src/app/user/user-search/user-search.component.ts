c8777fdc0459549e16c737222572e25aca9880df
piotrekkk23 <gppiotrekgp23@gmail.com>
2023-02-16 09:51:41 +0100

    user search cleanup and formatting

--- open-e-recruitment-task/src/app/user/user-search/user-search.component.ts
+++ open-e-recruitment-task/src/app/user/user-search/user-search.component.ts
@@ -1,9 +1,5 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { User } from '../user.model';
-import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user-search',
@@ -11,28 +7,13 @@ import { UserService } from '../user.service';
   styleUrls: ['./user-search.component.scss']
 })
 export class UserSearchComponent {
-  @ViewChild('f',{static: true}) searchForm!: NgForm;
   userIndex!: number;
 
 
   onSubmit():void {
-
     this.router.navigate(['/user/' + this.userIndex]);
-    
-      // this.userService.searchUser(this.userIndex).subscribe({
-      //   next: () =>{
-      //     this.router.navigate(['/user/' + this.userIndex]);
-      //   },
-      //   error: (error) => {
-      //     if (error.status === 404){
-      //       this.router.navigate(['/user/' + this.userIndex]);
-      //     } else {
-      //       //display error message
-      //     }
-      //     }
-      // });
   }
 
-  constructor(private userService: UserService, private router: Router){}
+  constructor(private router: Router){}
 
 }
